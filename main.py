a6b77deb3047523a55a83f41bfc65bb8b87944dc
Jarlan Silva de Lima <36967724+jarlanjsl@users.noreply.github.com>
2024-12-19 23:35:59 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+from lib import download_dataset
+from lib import preencher_matriz_contratos
+from lib import imprimir_matriz
+from lib import exportar_csv
+
+def main():
+    
+    # Download do dataset
+    url = "https://drive.google.com/file/d/1YFPo-k9yyzitXp6HTfCfbselD1DUSJ4K/view?usp=drive_link"
+    filename = 'T1/dataset/entrada.txt'
+    download_dataset(url,filename)
+
+    # Preencher a matriz de contratos
+    m, n, t, matriz = preencher_matriz_contratos(filename)
+    
+    # Imprimir os resultados
+    print(m, n, t, "\n")
+    imprimir_matriz(matriz)
+
+    # Exportar a matriz de contratos
+    file_csv = "T1/dataset/contratos_teste.csv"
+    exportar_csv(matriz, file_csv)
+    
+if __name__ == "__main__":
+    main()
